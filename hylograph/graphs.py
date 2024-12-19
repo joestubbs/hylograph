90d670ac92f5941c5008e3f46da90ada4df39f6b
Joe Stubbs <joestubbs5@gmail.com>
2024-12-19 11:55:50 -0600

    Add additional graphs to execute cypher

--- hylograph/graphs.py
+++ hylograph/graphs.py
@@ -1,9 +1,10 @@
+from functools import partial
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import StateGraph, START, END
 
 from nodes import *
 from edges import *
-from states import Text2QueryGraphState
+from states import Text2QueryGraphState, Text2QueryGraphExecuteState
 
 
 # a basic text2query graph
@@ -16,6 +17,7 @@ text2query_graph_builder.add_node("validate_cypher", node_validate_cypher)
 text2query_graph_builder.add_node("repair_cypher", node_repair_cypher)
 
 # Add edges 
+edge_query_has_error = partial(partial_edge_query_has_error, END)
 text2query_graph_builder.add_edge(START, "generate_examples")
 text2query_graph_builder.add_edge("generate_examples", "generate_cypher")
 text2query_graph_builder.add_edge("generate_cypher", "validate_cypher")
@@ -36,6 +38,7 @@ text2query_similarity_graph_builder.add_node("validate_cypher", node_validate_cy
 text2query_similarity_graph_builder.add_node("repair_cypher", node_repair_cypher)
 
 # Add edges 
+edge_query_has_error = partial(partial_edge_query_has_error, END)
 text2query_similarity_graph_builder.add_edge(START, "get_question_similarity")
 text2query_similarity_graph_builder.add_conditional_edges("get_question_similarity", 
                                                           edge_has_query,
@@ -49,6 +52,35 @@ text2query_similarity_graph_builder.add_conditional_edges("validate_cypher",
                                      END: END})
 
 
+# a text2query graph that includes a similarity checker node and a node to actually 
+# execute the cypher. 
+text2query_sim_graph_execute_builder = StateGraph(Text2QueryGraphExecuteState)
+
+# add nodes
+text2query_sim_graph_execute_builder.add_node("get_question_similarity", node_get_question_similarity)
+text2query_sim_graph_execute_builder.add_node("generate_examples", node_filter_neo4j_examples)
+text2query_sim_graph_execute_builder.add_node("generate_cypher", node_generate_cypher)
+text2query_sim_graph_execute_builder.add_node("validate_cypher", node_validate_cypher)
+text2query_sim_graph_execute_builder.add_node("repair_cypher", node_repair_cypher)
+text2query_sim_graph_execute_builder.add_node("execute_cypher", node_execute_cypher)
+
+# Add edges 
+edge_query_has_error = partial(partial_edge_query_has_error, "execute_cypher")
+text2query_sim_graph_execute_builder.add_edge(START, "get_question_similarity")
+text2query_sim_graph_execute_builder.add_conditional_edges("get_question_similarity", 
+                                                          edge_has_query,
+                                                          {"generate_examples": "generate_examples",
+                                                           "validate_cypher": "validate_cypher"})
+text2query_sim_graph_execute_builder.add_edge("generate_examples", "generate_cypher")
+text2query_sim_graph_execute_builder.add_edge("generate_cypher", "validate_cypher")
+
+text2query_sim_graph_execute_builder.add_conditional_edges("validate_cypher", 
+                                    edge_query_has_error,
+                                    {"repair_cypher": "repair_cypher",
+                                     "execute_cypher": "execute_cypher"})
+text2query_sim_graph_execute_builder.add_edge("execute_cypher", END)
+
+
 # Remember results of graph executions within the same thread
 memory = MemorySaver()
 
@@ -60,3 +92,5 @@ memory = MemorySaver()
 text2query_graph = text2query_graph_builder.compile()
 
 text2query_sim_graph = text2query_similarity_graph_builder.compile()
+
+text2query_sim_graph_execute = text2query_sim_graph_execute_builder.compile()
